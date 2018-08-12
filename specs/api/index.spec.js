9372e4be4a9949ac6f47d7506f76eb1082a987fa
v.yandimirkin <v.yandimirkin@rambler-co.ru>
2018-08-12 20:15:25 +0300

    feat: review api post tests

--- specs/api/index.spec.js
+++ specs/api/index.spec.js
@@ -3,6 +3,13 @@ const fetch = require('node-fetch');
 const PORT = process.env.SERVER_PORT;
 
 describe('API Node.js', () => {
+  const body = {
+    name: 'Elvis Presley',
+    email: 'theking@elvismansion.com',
+    productid: '4',
+    review: 'I really love the product and will recommend!',
+  };
+
   it('should return 404 if unexisted path', (done) => {
     fetch('http://localhost:' + PORT + '/' + Math.random())
       .then((resp) => {
@@ -37,20 +44,31 @@ describe('API Node.js', () => {
   });
 
   it('should return 200 if ok body', (done) => {
-    const body = {
-      name: 'Elvis Presley',
-      email: 'theking@elvismansion.com',
-      productid: '8',
-      review: 'I really love the product and will recommend!',
-    };
-
     fetch('http://localhost:' + PORT + '/api/reviews', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(body),
     })
+      .then((text) => text.json())
       .then((resp) => {
-        expect(resp.status).to.equal(200);
+        expect(resp.success).to.equal(true);
+        expect(resp.reviewId).to.be.a('number');
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should return success false if productid not exists', (done) => {
+    let unexisted = JSON.parse(JSON.stringify(body));
+    unexisted.productid = 0;
+    fetch('http://localhost:' + PORT + '/api/reviews', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(unexisted),
+    })
+      .then((text) => text.json())
+      .then((body) => {
+        expect(body.success).to.equal(false);
         done();
       })
       .catch(done);
