d05845f2a54a1f5493bc503a83c82a4bc5d23e3e
v.yandimirkin <v.yandimirkin@rambler-co.ru>
2018-08-12 20:13:15 +0300

    feat: PostgreSQL connect in API

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -4,9 +4,19 @@ const bodyParser = require('body-parser');
 /** router using express */
 const express = require('express');
 const app = express();
+const reviewsRoute = require('./routes/reviews');
+const {connectPool} = require('../commons/postgresConnect');
+
+connectPool()
+.then(() => {
+  logger.info('Connected to Postgres...');
+})
+.catch((error) => {
+  logger.error(error);
+});
+
 /** disable x-powered-by: express */
 app.disable('x-powered-by');
-const reviewsRoute = require('./routes/reviews');
 
 /** Some env parameters */
 const PORT = process.env.SERVER_PORT || 8888;
@@ -23,24 +33,6 @@ app.use('/' + API_PREFIX, reviewsRoute);
 
 /** Answer empty 404 if no route found */
 app.use((_, res) => res.sendStatus(404));
-
-const {Pool, getReviews} = require('../commons/models/review');
-
-Pool.connect()
-  .then((client) => {
-    getReviews(client)
-      .then((result) => {
-        logger.warn(JSON.stringify(result));
-        client.release();
-      })
-      .catch((error) => {
-        logger.error(error, 0);
-        client.release();
-      });
-  })
-  .catch((error) => {
-    logger.error(error, 1);
-  });
 
 /** Listen PORT for incoming requests */
 app.listen(PORT, () => {
